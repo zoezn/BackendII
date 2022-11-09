850c056b6b675fd3f2a77ec7a47c1342b7523843
Zoe} <zxejimenez@gmail.com>
2022-11-09 16:42:35 -0300

    Actualizando repo

---
+++ C10/config/src/test/java/com/zoezn/taller/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoezn.taller.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- C5/banco-service/src/main/java/com/zoezn/bancoservice/BancoController.java
+++ C5/banco-service/src/main/java/com/zoezn/bancoservice/BancoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -18,21 +19,18 @@ public class BancoController {
     private String[] denominaciones;
 
     @RequestMapping(method = RequestMethod.GET, path ="/banco/caja/efectivo")
-    public Map<String, String> mensajeEfectivo() {
-        Map<String, String> response = new HashMap<>();
+    public String mensajeEfectivo() {
 
-        response.put("Maximo efectivo permitido", maxEfectivo);
-
+        String response="Maximo efectivo permitido " + maxEfectivo;
 
         return response;
     }
     @RequestMapping(method = RequestMethod.GET, path ="/banco/caja/denominaciones")
     public Map<String, String> mensajeDenominaciones() {
         Map<String, String> response = new HashMap<>();
-
         int i = 1;
         for (String d:denominaciones) {
-            response.put("Denominacion" + i, d);
+            response.put("Denominacion"+ i, d);
             i++;
         }
 
